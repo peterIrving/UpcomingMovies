ffde2e631d3db91f7dc57b45faa9a9db7ceddfc7
peter irving <irving.peter92@gmail.com>
2021-07-29 09:23:22 -0500

    Initial Commit

---
+++ UpcomingMovies/UpcomingMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UpcomingMoviesApp.swift
+//  UpcomingMovies
+//
+//  Created by Peter Irving on 7/29/21.
+//
+
+import SwiftUI
+
+@main
+struct UpcomingMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
